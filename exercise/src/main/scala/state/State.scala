8bb88718b7812085d2843b7f5cb57590a63a9a6c
ryskit <ryosukekida.71@gmail.com>
2021-05-06 22:44:41 +0900

    done ~ EXERCISE6.10

--- exercise/src/main/scala/state/State.scala
+++ exercise/src/main/scala/state/State.scala
@@ -1,6 +1,7 @@
 package state
 
-import scala.annotation.tailrec
+import State._
+import state.RNG.map2
 
 trait RNG {
   def nextInt: (Int, RNG)
@@ -99,4 +100,91 @@ object RNG {
   // map を使って double をもう少し要領よく実装し直せ。EXERCISE6.2を参照。
   def doubleUseMap: Rand[Double] =
     map(nonNegativeInt)(_ / Int.MaxValue.toDouble + 1)
+
+  // EXERCISE6.6
+  // 以下のシグネチャに基づいて map2 を実装せよ。この関数は、 ra と rb の2つのアクションと、
+  // それらの結果を結合する 関数f を受け取り、それらを結合する新しいアクションを返す。
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    rng => {
+      val (a, r1) = ra(rng)
+      val (b, r2) = rb(r1)
+      (f(a, b), r2)
+    }
+
+  // EXERCISE6.7
+  // [難問] 2つのRNG遷移の組み合わせが可能であるとしたら、それらのリスト全体を結合することも可能であるはずだ。
+  // 遷移のListを1つの遷移にまとめるための sequence を実装せよ。それを使って、以前に記述した ints関数 を再実装せよ。
+  // その際には、標準ライブラリのList.fill(n)(x)関数 を使って x を n 回繰り返すリストを作成できる。
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
+    fs.foldRight(unit(List[A]()))((f, acc) => map2(f, acc)(_ :: _))
+
+  def intsUseSequence(count: Int): Rand[List[Int]] = {
+    sequence(List.fill(count)(int))
+  }
+
+  def both[A, B](ra: Rand[A], rb: Rand[B]): Rand[(A, B)] =
+    map2(ra, rb)((_, _))
+
+  def nonNegativeLessThan_(n: Int): Rand[Int] = { rng =>
+    val (i, rng2) = nonNegativeInt(rng)
+    val mod = i % n
+    if (i + (n - 1) - mod >= 0)
+      (mod, rng2)
+    else nonNegativeLessThan(n)(rng)
+  }
+
+  // EXERCISE6.8
+  // flatMap を実装し、それを使って nonNegativeLessThan 実装せよ。
+  def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] =
+    rng => {
+      val (a, r1) = f(rng)
+      g(a)(r1)
+    }
+
+  def nonNegativeLessThan(n: Int): Rand[Int] =
+    flatMap(nonNegativeInt) { i =>
+      val mod = i % n
+      if (i + (n - 1) - mod >= 0) unit(mod) else nonNegativeLessThan(n)
+    }
+
+  // EXERCISE6.9
+  // flatMap を使って map と map2 を再実装せよ。
+  // これが可能であることは、flatMap がmap と map2 よりも強力であると述べていることから明らかである。
+  def mapUseFlatMap[A, B](ar: Rand[A])(f: A => B): Rand[B] =
+    flatMap(ar)(a => unit(f(a)))
+
+  def map2UseFlatMap[A, B, C](ar: Rand[A],
+                              br: Rand[B])(f: (A, B) => C): Rand[C] =
+    flatMap(ar) { a =>
+      map(br)(b => f(a, b))
+    }
+
+  def rollDie: Rand[Int] = mapUseFlatMap(nonNegativeLessThan(6))(_ + 1)
+}
+
+case class State[S, +A](run: S => (A, S)) {
+  def map[B](f: A => B): State[S, B] =
+    flatMap(a => unit(f(a)))
+
+  def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
+    flatMap(a => sb.map(b => f(a, b)))
+
+  def flatMap[B](f: A => State[S, B]): State[S, B] =
+    State(s => {
+      val (a, s1) = s
+      f(a).run(s1)
+    })
+
+}
+
+object State {
+  type Rand[A] = State[RNG, A]
+
+  // EXERCISE6.10
+  // unit, map, map2, flatMap, sequence の5つの関数を一般化せよ。
+  // 可能であれば、それらをStateケースクラスのメソッドとして追加せよ。それが不可能であれば、State コンパニオンオブジェクトに配置せよ。
+  def unit[S, A](a: A): State[S, A] = State(s => (a, s))
+
+  def sequence[S, A](l: List[State[S, A]]): State[S, List[A]] =
+    l.foldRight(unit[S, List[A]](List[A]()))((f, acc) => f.map2(acc)(_ :: _))
 }
